4cb61d36e2d8c8745652326509ef48450cb2c344
Istiyak <istiyak.riyad@gmail.com>
2022-10-03 16:59:34 +0600

    Update the search

--- server/routes/search/index.js
+++ server/routes/search/index.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const Book = require('../../models/book');
+const Author = require('../../models/author');
+const Publisher = require('../../models/publisher');
+
 
 router.post('/', async (req, res, next) => {
     try {
         const {
             search_query,
             sort,
-            category,
             ratings
         } = req.body;
 
@@ -24,7 +26,6 @@ router.post('/', async (req, res, next) => {
 
         const books = await Book.find({
                 name: {$in: subQueries},
-                subCategory: {$in: category},
                 $or: ratings.map(rating => ({rating: {$gte: rating, $lt: Math.floor(rating + 1)}}))
             }, {_id: 1, name: 1, image: 1, rating: 1, numberOfRating: 1, writer: 1, pdfFile: 1, audioFile: 1})
             .sort({
@@ -33,10 +34,31 @@ router.post('/', async (req, res, next) => {
                 name: name ? 1 : -1,
             })
             .populate('writer', '_id name');
+
+        const authors = await Author.find({
+                name: {$in: subQueries},
+            }, {_id: 1, name: 1, image: 1})
+            .sort({
+                _id: date ? 1 : -1,
+                name: name ? 1 : -1,
+            });
+
+        const publishers = await Publisher.find({
+                name: {$in: subQueries},
+            }, {_id: 1, name: 1, image: 1})
+            .sort({
+                _id: date ? 1 : -1,
+                name: name ? 1 : -1,
+            });
+        
         
         res.json({
             message: 'Filter and search data',
-            books
+            data: {
+                books,
+                authors,
+                publishers
+            }
         });
 
     }
