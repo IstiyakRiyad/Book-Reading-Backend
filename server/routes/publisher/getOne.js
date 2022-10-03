9873f5c45a4c73ae8bb0c811ffb60fd59a1cd5cd
Istiyak <istiyak.riyad@gmail.com>
2022-10-03 16:05:47 +0600

    Add authors in publisher all route

--- server/routes/publisher/getOne.js
+++ server/routes/publisher/getOne.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Publisher = require('../../models/publisher');
+const Author = require('../../models/author');
 const Book = require('../../models/book');
 const createError = require('http-errors');
 
@@ -13,11 +14,16 @@ router.get('/:id', async (req, res, next) => {
         if(!publisher) throw createError(404, 'Publisher not found');
 
         // TODO: Adding publisher books
-        const books = await Book.find({publisher: publisher._id}, {_id: 1, name: 1, image: 1, rating: 1, numberOfRating: 1});
+        const books = await Book.find({publisher: publisher._id}, {_id: 1, name: 1, image: 1, rating: 1, numberOfRating: 1, writer: 1});
 
+        let writersIds = books.map(book => book.writer);
+        writersIds = [...new Set(writersIds)];
+
+        const writers = await Author.find({_id: {$in: writersIds}}, {_id: 1, name: 1, image: 1});
 
         const publisherData = publisher.toJSON();
         publisherData.books = books;
+        publisherData.writers = writers;
         
         
         res.json({
